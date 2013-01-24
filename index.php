27727218f3ac2dedd6c460d8d09a780c368791ce
Alex Sychev <194145@gmail.com>
2013-01-24 19:53:12 +0400

    Create custom templates.

--- index.php
+++ index.php
@@ -1,28 +1,53 @@
 <?php
     use \Michelf\MarkdownExtra;
 
+function string_starts_with($haystack, $needle) {
+    $pos = strpos($haystack, $needle);
+    if ($pos === 0) {
+        return true;
+    }
+    return false;
+}
+
 function print_page_content_with_style($md_page_content = "") {
     require("settings.php");
     include("php-markdown/Markdown.php");
     include("php-markdown/MarkdownExtra.php");
     
-    $style_file_name = "./themes/" . $settings_array['style']['theme'] . "/index.html";
-    $style_exists = file_exists($style_file_name);
-        if($style_exists) {
-        // print("Main style file name is <code>" . $style_file_name . "</code>");
-        $style_file_content = file_get_contents($style_file_name);
-        if ($style_file_content != FALSE) {
+    $template_file_name = "index.html";
+    
+    // check if we have to show special template
+    foreach($settings_array as $key => $value) {
+        // print($key . " => " . $value . "<br />");
+        if (string_starts_with($key, "custom-style") && is_array($value)) {
+            if (array_key_exists("url_starts_with", $value) && // check that we have url property for custom template
+                string_starts_with($_SERVER['REQUEST_URI'], $value["url_starts_with"]) && // check that first lettels of our url the same like custom template url definition
+                array_key_exists("custom_template", $value)) { // chechk that we have file name for the custom template
+                
+                // print("We found custom template information for " . $value["url_starts_with"] . " " . $value["custom_template"]);
+                $template_file_name = $value["custom_template"];
+            }
+        }
+    }
+    
+    $template_file_path = "./themes/" . $settings_array['style']['theme'] . "/" . $template_file_name;
+
+    $template_exists = file_exists($template_file_path);
+    if($template_exists) {
+        // print("Main style file name is <code>" . $template_file_path . "</code>");
+        $template_file_content = file_get_contents($template_file_path);
+        if ($template_file_content != FALSE) {
             // we can read style content
-            $stylized_page = str_replace("<!--php-md-article-content-here-->", MarkdownExtra::defaultTransform($md_page_content), $style_file_content);
+            $stylized_page = str_replace("<!--php-md-article-content-here-->", MarkdownExtra::defaultTransform($md_page_content), $template_file_content);
             
             header("Content-Type: text/html");
             print($stylized_page);
         } else {
-            print("Can't read style file file at " . $style_file_name . "<br />");
+            print("Can't read style file file at " . $template_file_path . "<br />");
             // something goes wrong: we can't read style file
         }
     } else {
-        print("Can't find style file file at " . $style_file_name . "<br />");
+        print("Can't find style file file at " . $template_file_path . "<br />");
     }
 }
 
