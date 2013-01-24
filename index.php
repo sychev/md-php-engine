fcd3a3702a7266db63126d30e300d3b3fddb2344
Alex Sychev <194145@gmail.com>
2013-01-24 13:30:49 +0400

    Add simple style settings in settings.ini.

--- index.php
+++ index.php
@@ -34,4 +34,22 @@ if (isset($_GET['page'])) {
     print("Page is not set.<br />");
     print_page_from_md();
 }
+
+$default_settings_array = array(
+    "style" => array(
+        "theme" => "typo",
+    ),
+);
+
+$settings_array = $default_settings_array;
+
+// Parse settings file
+$settings_array = parse_ini_file("settings.ini", true);
+//print_r($settings_array);
+if (array_key_exists("style", $settings_array)) {
+    $style_array = $settings_array["style"];
+    if (array_key_exists("theme", $style_array)) {
+        print("Theme is " . $style_array["theme"]);
+    }
+}
 ?>
